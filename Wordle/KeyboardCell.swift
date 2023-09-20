4f03907685af94992cab531e50ee7a69c2d645dd
Kenny Smith <itskhru@Kennys-MacBook-Pro.local>
2023-09-20 19:51:08 -0400

    Finished Steps 1 and 2

--- Wordle/KeyboardCell.swift
+++ Wordle/KeyboardCell.swift
@@ -30,15 +30,20 @@ class KeyboardCell: UICollectionViewCell {
       Checkpoint: After finishing this exercise, you should now be able to see the keyboard laid out on the screen! If it's not working, check your work on exercises 1 & 2.
     */
     // START YOUR CODE HERE
-    // ...
-    // END YOUR CODE HERE
+      self.string = string
+      label.text = string
+      // END YOUR CODE HERE
   }
 
   // Exercise 5: Call the `didSelectString` closure and pass in the string this cell holds (see line 14)
   // Checkpoint: After finishing this exercise, you should now be able to tap on a keyboard cell and have the letter shown on the board! If it's not working, check your work on exercises 3,4, and 5.
   @objc private func didTapString() {
     // START YOUR CODE HERE
-    // ...
-    // END YOUR CODE HERE
+      if let didSelectString = didSelectString {
+          //Call the closure and pass in the string this cell holds
+          didSelectString(self.string)
+      }
+      // END YOUR CODE HERE
   }
+    
 }
