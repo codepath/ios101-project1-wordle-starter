13f11ced63557f0cde7f19d5a8cdb0201f0a7089
Micahscodingjourney <117372632+Micahscodingjourney@users.noreply.github.com>
2024-02-27 00:50:41 -0500

    solved exercise 1

--- Wordle/KeyboardController.swift
+++ Wordle/KeyboardController.swift
@@ -30,7 +30,7 @@ class KeyboardController: NSObject,
     // Exercise 1: Return the correct number of items in a section
     // Tip: There's a helper method you can use located in this class
     // START YOUR CODE HERE
-    return 0
+    return numItems(in: section)
     // END YOUR CODE HERE
   }
 
